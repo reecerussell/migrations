d2f36b84024b4049908d7c1dab5153170d511612
Reece Russell <reece@prominentmedia.com>
2021-02-22 17:40:39 +0000

    remove unused code

--- provider.go
+++ provider.go
@@ -2,13 +2,6 @@ package migrations
 
 import (
 	"context"
-	"sync"
-)
-
-// In-memory store of registered migration providers.
-var (
-	mu   sync.Mutex
-	prvs []Provider
 )
 
 // Provider is used to interface with a database system, such as SQL Server,
